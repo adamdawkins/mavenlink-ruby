adaf46bec6f2a581a4bd7fbd573a745a9a864554
adamdawkins <adam@dragondrop.uk>
2020-03-15 13:22:21 +0000

    ♻️  Spec simplification

--- spec/lib/mavenlink/resources/invoice_spec.rb
+++ spec/lib/mavenlink/resources/invoice_spec.rb
@@ -3,23 +3,19 @@
 require_relative "../../../spec_helper"
 
 RSpec.describe Mavenlink::Invoice, type: :model do
-  describe ".retrieve" do
-    it "should be retrievable", :vcr do
-      invoice = Mavenlink::Invoice.retrieve("8185325")
+  it "should be retrievable", :vcr do
+    invoice = Mavenlink::Invoice.retrieve("8185325")
 
-      expect(a_request(:get, "#{Mavenlink.api_base}/invoices/8185325")).to(
-        have_been_made
-      )
-      expect(invoice).to be_a Mavenlink::Invoice
-    end
+    expect(a_request(:get, "#{Mavenlink.api_base}/invoices/8185325")).to(
+      have_been_made
+    )
+    expect(invoice).to be_a Mavenlink::Invoice
   end
 
-  describe ".list" do
-    it "should be listable", :vcr do
-      invoices = Mavenlink::Invoice.list
-      expect(a_request(:get, "#{Mavenlink.api_base}/invoices")).to have_been_made
-      expect(invoices).to be_a Mavenlink::List
-      expect(invoices.first).to be_a Mavenlink::Invoice
-    end
+  it "should be listable", :vcr do
+    invoices = Mavenlink::Invoice.list
+    expect(a_request(:get, "#{Mavenlink.api_base}/invoices")).to have_been_made
+    expect(invoices).to be_a Mavenlink::List
+    expect(invoices.first).to be_a Mavenlink::Invoice
   end
 end
