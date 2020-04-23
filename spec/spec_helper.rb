5cdbde6295dd9afda0c592d020a70182ac14f694
adamdawkins <adam@dragondrop.uk>
2020-04-23 12:49:03 +0100

    🚨 Remove API Token from test fixtures

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,7 @@ VCR.configure do |config|
   config.cassette_library_dir = "fixtures/vcr_cassettes"
   config.hook_into :webmock
   config.configure_rspec_metadata!
+  config.filter_sensitive_data("<API_TOKEN>") { ENV["MAVENLINK_API_KEY"] }
 end
 
 RSpec.configure do |config|
